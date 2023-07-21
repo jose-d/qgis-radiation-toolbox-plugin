8f5c39fd990d45d12a406c5f693ff83cf85c37a8
Martin Landa <landa.martin@gmail.com>
2023-07-21 21:24:22 +0200

    update build packager, see #67

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -1,24 +1,6 @@
 #!/bin/bash -e
 
-# Custom plugin builder
-
-DIR=`pwd`/..
-
-pythonqwt_packager() {
-    TMPDIR=/tmp/pythonqwt
-    python3 -m venv /tmp/pythonqwt
-    source /tmp/pythonqwt/bin/activate
-    python3 -m pip install PythonQwt
-    cp -r $TMPDIR//lib/python3.9/site-packages/qwt/ $DIR/
-    rm -rf $TMPDIR
-}
-
-packager() {
-    find $DIR | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
-    pb_tool zip
-}
-
-pythonqwt_packager
-packager
+find . -name __pycache__ | xargs rm -rf
+pb_tool zip
 
 exit 0
